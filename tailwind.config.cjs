6988df34d5838d50527140a3081d089d2b1d39f8
Zara <Zara>
2024-02-29 22:11:11 +0100

    ye

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-  };
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+  theme: {
+    extend: {
+      colors: {
+        reod1: "#461220",
+        reod2: "#8C2F39",
+        reod3: "#B13A48",
+        reod4: "#E0003C",
+        pinky: "#FCB9B2",
+        back: "#FED0BB",
+      },
+      fontFamily: {
+        cinzel: ["Cinzel"],
+        rokkitt: ["Rokkitt"],
+      },
+    },
+  },
+};
